64a07280207b5a611afefba92e945c186bf99dc3
Yno-gasai <67794190+Yno-gasai@users.noreply.github.com>
2020-07-04 00:25:31 +0300

    room

---
+++ космос/js/script.js
@@ -0,0 +1,74 @@
+alert("Сайт Артёма ,ну и в частности конечно же Хацкера");
+jQuery(function ($) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 274) {
+            $('#navigation').addClass('fixed');
+        } else if ($(this).scrollTop() < 274) {
+            $('#navigation').removeClass('fixed');
+        }
+    });
+});
+$(document).ready(function() {
+  var navOpen = false;
+  $('.toggle-nav').click(function() {
+    if (navOpen == false) {
+      $('.wrapper').css("position", "absolute").animate({
+        left: "30%"
+      });
+      $(this).animate({
+        left: "30%"
+      }).removeClass("entypo-menu").addClass("entypo-cancel");
+      $('nav').animate({
+        left: "0%"
+      });
+      navOpen = true;
+    } else {
+      $('.wrapper').animate({
+        left: "0%"
+      }, function() {
+        $(this).css("position", "relative");
+      });
+      $(this).animate({
+        left: "0%"
+      }).removeClass("entypo-cancel").addClass("entypo-menu");
+      $('nav').animate({
+        left: "-30%"
+      });
+      navOpen = false;
+    }
+  });
+
+  // Smooth Anchor Scrolling
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+    return false;
+  });
+});
+
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() != 0) {
+
+            $('#knopohka_wtf').fadeIn();
+
+        } else {
+
+            $('#knopohka_wtf').fadeOut();
+
+        }
+
+    });
+
+    $('#knopohka_wtf').click(function () {
+
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+
+    });
+
+});
